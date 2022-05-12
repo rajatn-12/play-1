b5bc357a7d21afca0a04e1537e55ffd7d95b83e9
Rajat Nayak <Rajat.Nayak@teknopoint.local>
2022-05-12 13:40:35 +0530

    my Pages

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   useEffect(() => {
     if (isAndroid) {
       const url =
-        "https://play.google.com/store/apps/details?id=com.instagram.android&referrer=utm_source%3Dinstagramweb%26utm_campaign%3DloginPage%26ig_mid%3DF5261F93-D02A-4200-85FC-6548A4F73F91%26utm_content%3Dlo%26utm_medium%3Dbadge";
+        "intent://instagram.com/#Intent;scheme=https;package=com.instagram.android;end";
 
       window.location.replace(url);
     } else if (isIOS) {
@@ -19,7 +19,7 @@ function App() {
         );
       }, 10000);
     } else {
-      window.location.replace("https://www.bajajfinserv.in/");
+      window.location.replace("https://instagram.com");
     }
   }, []);
   return (
@@ -28,7 +28,7 @@ function App() {
         If you have not been automatically redirected, click on the following link:
       </div>
       {isAndroid ? (
-        <a href="https://play.google.com/store/apps/details?id=com.instagram.android">
+        <a href="https://play.google.com/store/apps/details?id=com.instagram.android&referrer=utm_source%3Dinstagramweb%26utm_campaign%3DloginPage%26ig_mid%3DF5261F93-D02A-4200-85FC-6548A4F73F91%26utm_content%3Dlo%26utm_medium%3Dbadge">
           Open Android app
         </a>
       ) : isIOS ? (
@@ -36,7 +36,7 @@ function App() {
           Open iOS app
         </a>
       ) : (
-        <a href="https://www.bajajfinserv.in/">Open Web app</a>
+        <a href="https://instagram.com">Open Web app</a>
       )}
     </div>
   );
